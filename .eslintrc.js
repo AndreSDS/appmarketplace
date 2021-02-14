21dab79b773f9d6afbc1b0dcf9ee2e0715934443
AndreSDS <andre_smiths@outlook.com>
2021-02-14 12:16:00 -0300

    iniciando projeto e configs do iniciais

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks',
+    'jsx-ally',
+    'import',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extensions' :[
+      'warn',
+      {
+        extensions: ['.jsx', '.js']
+      }
+    ],
+    'import/prettier-default-export': 'off',
+    'no-parse-reassign': 'off'
+  },
 };
