ff7676d8e760ea4b40831b25721be9bb9f38295b
AndreSDS <andre_smiths@outlook.com>
2021-02-14 16:20:15 -0300

    cirando component que lista produtos e alterando build gradlew

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Cart = () => {
+  return (
+    <Container>
+      <Text>Cart</Text>
+    </Container>
+  );
+};
+
+export default Cart;
